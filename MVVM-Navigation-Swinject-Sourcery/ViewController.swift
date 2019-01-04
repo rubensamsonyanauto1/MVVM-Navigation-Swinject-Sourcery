6d07cabaee32607dba101bd54ef121e886efd124
Ruben <ruben.samsonyan@auto1.com>
2019-01-04 16:32:18 +0100

    Initial Commit

---
+++ MVVM-Navigation-Swinject-Sourcery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Navigation-Swinject-Sourcery
+//
+//  Created by Ruben Samsonyan on 04.01.19.
+//  Copyright © 2019 Ruben Samsonyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
